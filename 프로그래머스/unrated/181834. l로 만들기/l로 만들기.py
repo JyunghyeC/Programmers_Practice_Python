ba5a6a8d20dca070f0482ed1a4104c0782fa2f8d
JyunghyeC <126124026+JyunghyeC@users.noreply.github.com>
2023-08-02 00:15:37 +0900

    [unrated] Title: l로 만들기, Time: 49.76 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -1,6 +1,5 @@
 def solution(myString):
-    before = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
     for i in myString:
-        if i in before:
+        if ord(i) < ord("l"):
             myString = myString.replace(i, "l")  
     return myString
